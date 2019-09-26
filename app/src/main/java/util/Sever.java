ea73d8c728fef91a833851d45a9d20344700ece9
NTD98 <37630367+NTD98@users.noreply.github.com>
2019-09-26 10:58:59 +0700

    init App

---
+++ app/src/main/java/util/Sever.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Sever {
+    public static String localhost = "172.30.87.247";
+    public static String producttypelink = "http://"+localhost+ "/Sever/getdevicetype.php";
+    public static String newproduct = "http://"+localhost+":80/Sever/getnewproduct.php";
+    public static String product = "http://"+localhost+":80/Sever/getproduct.php";
+    public static String billlink = "http://"+localhost+":80/Sever/customerinformation.php";
+    public static String orderdetaillink = "http://"+localhost+":80/Sever/orderdetail.php";
+    public static String signup = "http://"+localhost+":80/Sever/signup.php";
+    public static String login = "http://"+localhost+":80/Sever/login.php";
+    public static String checkuser = "http://"+localhost+":80/Sever/checkuser.php";
+}
